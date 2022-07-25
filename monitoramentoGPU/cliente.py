8d04283df74dc3a9d004dd17a581327287d855a4
CassioJr <cassiojuniorceolin@gmail.com>
2022-07-25 14:38:50 -0300

    Adicionado comentarios + multiplos clientes
    Co-authored-by: Daniel Oliveira de Freitas <Daniel-Oliveira-de-Freitas@users.noreply.github.com>
    Co-authored-by: Emanuelle Leães Alves <emanuellela@users.noreply.github.com>

--- monitoramentoGPU/cliente.py
+++ monitoramentoGPU/cliente.py
@@ -1,10 +1,40 @@
 import socket
 
+# Informações sobre o IP e a porta do servidor
 HOST = "127.0.0.1"
-PORT= 1024
+PORT= 2222
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect((HOST,PORT))
-buffer = server.recv(1024)
-buffer = buffer.decode("utf-8")
-print(f"{buffer}")
+# Metódo que cria um socket para a conexão
+# @Param AF_INET IPV4
+# @Param SOCK_STREAM Significa que é um protocolo TCP 
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as servidor:
+    # Realiza a conexão com as informações passadas do servidor
+    servidor.connect((HOST,PORT))
+    #Recebe as informações do servidor de quais ações poderá realizar
+    buffer = servidor.recv(1024)
+    buffer = buffer.decode("utf-8")
+    print(f"{buffer}")
+    op = input()
+
+    # Estrutura de decisão das mensagens que podem ser enviadas ao servidor
+    match op:
+        case '1':
+            servidor.sendall((bytes(op, "utf-8")))
+            data = servidor.recv(1024)
+            print(data.decode())
+        case '2':
+            servidor.sendall((bytes(op, "utf-8")))
+            data = servidor.recv(1024)
+            print(data.decode())
+        case '3':
+            servidor.sendall((bytes(op, "utf-8")))
+            data = servidor.recv(1024)
+            print(data.decode())
+        case '4':
+            servidor.sendall((bytes(op, "utf-8")))
+            data = servidor.recv(1024)
+            print(data.decode())
+        case default:
+            servidor.sendall((bytes(op, "utf-8")))
+            data = servidor.recv(1024)
+            print(data.decode())
--- monitoramentoGPU/servidor.py
+++ monitoramentoGPU/servidor.py
@@ -1,14 +1,71 @@
 import socket
-from monitoramentoGPU import Monitor
+import GPUtil
+from _thread import *
 
-HOST = "127.0.0.1"
-PORT= 1024
+class Servidor:
+    
+    # Metódo que inicializa as variaveis com o valor passado
+    # @param ip Endereço de ip do servidor
+    # @param porta Porta destinada ao servidor
+    def __init__(self, ip, porta):
+        self.ip = ip
+        self.porta = porta
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as servidor:
-    a= Monitor;
-    servidor.bind((HOST, PORT))
-    servidor.listen()
-    while True:
-        serv, adress = servidor.accept()
-        print(f"Conexão realizada: {adress[0]}:{adress[1]}")
-        serv.sendall(bytes(a.getGPUStats(), "utf-8"))
+    # Metodo que realiza a incialização do servidor 
+    def inicializar(self):
+        
+        # Metódo que cria um socket para a conexão
+        # @Param AF_INET IPV4
+        # @Param SOCK_STREAM Significa que é um protocolo TCP 
+        self.servidor_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.servidor_Socket.bind((self.ip, self.porta))
+        print('Servidor foi inicializado!')
+        self.servidor_Socket.listen(5)
+
+        # Método utilizado para pegar as GPUs disponiveis no computador
+        # return retorna a primeira GPU do array
+        def pegar_InfoGPU():
+            GPUs = GPUtil.getGPUs()
+            gpu = GPUs[0]
+            return gpu
+       
+        # Método que realiza a conexão com o cliente, onde são enviadas as informações de operações que podem ser realizadas
+        # Onde tambem é tratado da questao de multiplos clientes conectados
+        def cliente_thread(serv):
+            print(f"Conexão realizada com o cliente: {endereco[0]}:{endereco[1]}")
+            serv.sendall(bytes("\nDigite um número para receber os dados referentes a GPU: \n1- Para monitorar todos os dados \n2- Para monitorar o total de memória da GPU \n3- Para monitorar a memória livre e utilizada da GPU \n4- Para monitorar o consumo em '%' da GPU", "utf-8"))
+
+            while True:
+                data = serv.recv(1024)
+                # Estrutura de decisão para informar se um cliente foi desconectado
+                if not data:
+                    print(f"O cliente {endereco[0]}:{endereco[1]} foi desconectado")
+                    serv.close()
+                    break
+                op = data.decode()
+                
+                # Estrutura de decisões das mensagens que o servidor pode mandar para o cliente
+                # Retorna para o cliente o nome, total de memória, memória livre e utilizada e o total de uso da GPU
+                if op == "1":
+                    serv.sendall(bytes(f"\nNome da GPU: {pegar_InfoGPU().name}\n Total de Memória: {pegar_InfoGPU().memoryTotal}\n Memória livre: {pegar_InfoGPU().memoryFree}\n Memória utilizada: {pegar_InfoGPU().memoryUsed}\n Total de uso da GPU: {pegar_InfoGPU().memoryUtil:.2f} %", "utf-8"))
+                # Retorna para o cliente o total de memória da GPU
+                elif op == "2":
+                    serv.sendall(bytes(f"Total de Memória: {pegar_InfoGPU().memoryTotal}", "utf-8"))
+                # Retorna para o cliente o espaço de memória livre e utilizada da GPU
+                elif op == "3":
+                    serv.sendall(bytes(f"Memória livre: {pegar_InfoGPU().memoryFree}\n Memória utilizada: {pegar_InfoGPU().memoryUsed}", "utf-8"))
+                # Retorna para o cliente a porcentagem de uso da GPU
+                elif op == "4":
+                    serv.sendall(bytes(f"Total de uso da GPU: {pegar_InfoGPU().memoryUtil:.2f} %", "utf-8"))
+                # Retorna a mensagem que a operação é inválida caso o cliente mande qualquer outra coisa
+                # Que esteja fora dos opções válidas do menu 
+                else:
+                    serv.sendall(bytes(f"Operação Inválida!", "utf-8"))
+
+        while True:
+            # Aceita a conexão 
+            conn, endereco = self.servidor_Socket.accept()
+            start_new_thread(cliente_thread, (conn, ))
+
+serv = Servidor('127.0.0.1', 2222)
+serv.inicializar()
